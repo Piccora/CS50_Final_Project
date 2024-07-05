3d770236bcc7e3ec301757e22b43052f4eba0920
Piccora <tatuanminh31032004@gmail.com>
2024-07-05 17:08:08 +0700

    changed so Flask doesn't use filesystem session type anymore

--- virtual/include/app.py
+++ virtual/include/app.py
@@ -32,7 +32,7 @@ app.jinja_env.filters["usd"] = usd
 
 # Configure session to use filesystem (instead of signed cookies)
 app.config["SESSION_PERMANENT"] = False
-app.config["SESSION_TYPE"] = "filesystem"
+# app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
 # Configure application to use mongoDB database
